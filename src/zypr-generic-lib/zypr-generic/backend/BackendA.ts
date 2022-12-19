e0ccb0eb740d93207f70387e6bf49f9bca985dd4
Henry Blanchette <henry@monkeyface.com>
2022-12-18 20:20:03 -0800

    flushing out BackendA

--- src/zypr-generic-lib/zypr-generic/backend/BackendA.ts
+++ src/zypr-generic-lib/zypr-generic/backend/BackendA.ts
@@ -1,6 +1,21 @@
-import { Backend } from "../Backend";
+import * as Backend from "../Backend";
 import "../../../TypeCraft/Typescript/Interop"
+import * as Interop from "../../../TypeCraft/Typescript/Interop";
+import language from "../Language";
 
 // uses the iteroperations defined in TypeCraft/Typescript/Interop to build a
-// backend that calls the purescript backenbd
-export const backend: Backend = (() => { throw new Error("TODO") })()
+// backend that calls the purescript backend
+export const backend = (state: Interop.State): Backend.Backend => {
+  return {
+    props: {
+      language: language(),
+      format: (st: Interop.State) => {
+        throw new Error("TODO: use purescript backend");
+      },
+      handleKeyboardAction: (act: Backend.KeyboardAction) => (prop: Backend.Props, st: Interop.State) => {
+        throw new Error("TODO:");
+      }
+    },
+    state
+  }
+}
