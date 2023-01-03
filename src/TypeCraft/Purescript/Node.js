3078226d5540d20ccb866d8b8963ba685b3b9c54
Henry Blanchette <henry@monkeyface.com>
2023-01-03 11:47:03 +0900

    defined NodeTag

--- src/TypeCraft/Purescript/Node.js
+++ src/TypeCraft/Purescript/Node.js
@@ -5,6 +5,49 @@ export function makeNode_(node) {
   return node
 }
 
+// NodeTag
+
+export const makeArrowNodeTag = { case: 'ty arr' }
+export const makeTHoleNodeTag = { case: 'ty hol' }
+export const makeTNeuNodeTag = { case: 'ty neu' }
+export const makeForallNodeTag = { case: 'poly-ty forall' }
+export const makePTypeNodeTag = { case: 'poly-ty ty' }
+export const makeTypeArgNodeTag = { case: 'ty-arg' }
+export const makeAppNodeTag = { case: 'tm app' }
+export const makeLambdaNodeTag = { case: 'tm lam' }
+export const makeVarNodeTag = { case: 'tm var' }
+export const makeLetNodeTag = { case: 'tm let' }
+export const makeDataNodeTag = { case: 'tm dat' }
+export const makeTLetNodeTag = { case: 'tm ty-let' }
+export const makeTypeBoundaryNodeTag = { case: 'tm ty-boundary' }
+export const makeContextBoundaryNodeTag = { case: 'tm cx-boundary' }
+export const makeHoleNodeTag = { case: 'tm hol' }
+export const makeBufferNodeTag = { case: 'tm buf' }
+export const makeTypeBindNodeTag = { case: 'ty-bnd' }
+export const makeTermBindNodeTag = { case: 'tm-bnd' }
+export const makeCtrParamNodeTag = { case: 'ctr-prm' }
+export const makeConstructorNodeTag = { case: 'ctr' }
+
+// List TypeArg
+export const makeTypeArgListConsNodeTag = { case: 'ty-arg-list cons' }
+
+export const makeTypeArgListNilNodeTag = { case: 'ty-arg-list nil' }
+
+// List TypeBind
+export const makeTypeBindListConsNodeTag = { case: 'ty-bnd-list cons' }
+
+export const makeTypeBindListNilNodeTag = { case: 'ty-bnd-list nil' }
+
+// List Constructor
+export const makeConstructorListConsNodeTag = { case: 'ctr-list cons' }
+
+export const makeConstructorListNilNodeTag = { case: 'ctr-list nil' }
+
+// List CtrParam
+export const makeCtrParamListConsNodeTag = { case: 'ctr-prm-list cons' }
+
+export const makeCtrParamListNilNodeTag = { case: 'ctr-prm-list nil' }
+
 // NodeStyle
 
 export const makeNormalNodeStyle = { case: 'normal' }
@@ -25,5 +68,5 @@ export function makeNodeData_(data) {
 
 // NodeIndentation
 export const makeInlineNodeIndentation = { case: 'inline' }
-export const makeNewlineNodeIndentation  = { case: 'newline' }
-export const makeIndentNodeIndentation  = { case: 'indent' }
+export const makeNewlineNodeIndentation = { case: 'newline' }
+export const makeIndentNodeIndentation = { case: 'indent' }
--- src/TypeCraft/Purescript/Node.purs
+++ src/TypeCraft/Purescript/Node.purs
@@ -38,8 +38,11 @@ makeNode x =
 foreign import data NodeData :: Type
 
 foreign import data NodeIndentation :: Type
+
 foreign import makeInlineNodeIndentation :: NodeIndentation
+
 foreign import makeNewlineNodeIndentation :: NodeIndentation -- doesn't indent
+
 foreign import makeIndentNodeIndentation :: NodeIndentation
 
 foreign import makeNodeData_ ::
@@ -61,6 +64,77 @@ makeNodeData { indentation, isParenthesized, label } =
     , isParenthesized
     , label: pureNullable label
     }
+
+-- NodeTag
+foreign import data NodeTag :: Type
+
+-- NodeTag: Type
+foreign import makeArrowNodeTag :: NodeTag
+
+foreign import makeTHoleNodeTag :: NodeTag
+
+foreign import makeTNeuNodeTag :: NodeTag
+
+-- NodeTag: PolyType
+foreign import makeForallNodeTag :: NodeTag
+
+foreign import makePTypeNodeTag :: NodeTag
+
+-- NodeTag: TypeArg
+foreign import makeTypeArgNodeTag :: NodeTag
+
+-- NodeTag: Term
+foreign import makeAppNodeTag :: NodeTag
+
+foreign import makeLambdaNodeTag :: NodeTag
+
+foreign import makeVarNodeTag :: NodeTag
+
+foreign import makeLetNodeTag :: NodeTag
+
+foreign import makeDataNodeTag :: NodeTag
+
+foreign import makeTLetNodeTag :: NodeTag
+
+foreign import makeTypeBoundaryNodeTag :: NodeTag
+
+foreign import makeContextBoundaryNodeTag :: NodeTag
+
+foreign import makeHoleNodeTag :: NodeTag
+
+foreign import makeBufferNodeTag :: NodeTag
+
+-- NodeTag: TypeBind
+foreign import makeTypeBindNodeTag :: NodeTag
+
+-- NodeTag: TermBind
+foreign import makeTermBindNodeTag :: NodeTag
+
+-- NodeTag: CtrParam
+foreign import makeCtrParamNodeTag :: NodeTag
+
+-- NodeTag: Constructor
+foreign import makeConstructorNodeTag :: NodeTag
+
+-- NodeTag: List TypeArg
+foreign import makeTypeArgListConsNodeTag :: NodeTag
+
+foreign import makeTypeArgListNilNodeTag :: NodeTag
+
+-- NodeTag: List TypeBind
+foreign import makeTypeBindListConsNodeTag :: NodeTag
+
+foreign import makeTypeBindListNilNodeTag :: NodeTag
+
+-- NodeTag: List Constructor
+foreign import makeConstructorListConsNodeTag :: NodeTag
+
+foreign import makeConstructorListNilNodeTag :: NodeTag
+
+-- NodeTag: List CtrParam
+foreign import makeCtrParamListConsNodeTag :: NodeTag
+
+foreign import makeCtrParamListNilNodeTag :: NodeTag
 
 -- NodeStyle
 foreign import data NodeStyle :: Type
