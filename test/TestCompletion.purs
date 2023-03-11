eab24c180c4bb7e0a2719f372ffc7a749bb74e68
Henry Blanchette <henry@monkeyface.com>
2023-03-11 13:23:39 +0800

    path, term, type for each completion attempt as well

--- test/TestCompletion.purs
+++ test/TestCompletion.purs
@@ -2,6 +2,7 @@ module Test.TestCompletion where
 
 import Prelude
 import Prim hiding (Type)
+
 import Data.Foldable (traverse_)
 import Data.List (List(..))
 import Data.List as List
@@ -9,6 +10,7 @@ import Data.Maybe (Maybe(..))
 import Data.Tuple.Nested (type (/\), (/\))
 import Effect (Effect)
 import Effect.Class.Console as Console
+import Effect.Exception.Unsafe (unsafeThrow)
 import Test.Example.State1 (example1, example2)
 import Test.Logging (logBlock, logSectionEnd, logSectionStart)
 import TypeCraft.Purescript.Completions (calculateCompletionGroups)
@@ -70,6 +72,12 @@ testAllCompletions (tm /\ ty) = do
 testCompletion :: State -> Completion -> Effect Unit
 testCompletion st compl = do
   logSectionStart "testCompletion"
+  case st.mode of
+    CursorMode { cursorLocation: TermCursor _ctxs ty path tm } -> do
+      logBlock "path" (show path)
+      logBlock "term" (show tm)
+      logBlock "type" (show ty)
+    _ -> unsafeThrow $ "testCompletion: requires mode TermCursor, instead of actual mode: " <> show st.mode
   -- logBlock "state" (show st)
   logBlock "completion" (show compl)
   case st.mode of
