7672cd670452cb0e05da09ff14c5ba5b47721580
Jacob Prinz <jacobeliasprinz@gmail.com>
2023-03-19 18:26:41 -0400

    Default to var having arguments

--- src/TypeCraft/Purescript/Unification.purs
+++ src/TypeCraft/Purescript/Unification.purs
@@ -90,20 +90,20 @@ fillNeutralImpl actx pty id ty sub tyArgs = case pty of
   Forall x pty' ->
     let h = (freshTHole unit) in
     fillNeutralImpl actx pty' id ty sub{subTypeVars= Map.insert x h sub.subTypeVars} ((TypeArg defaultTypeArgMD h) List.: tyArgs)
-  PType ty' -> fillNeutral' actx (applySubType sub ty') id ty tyArgs
+  PType ty' -> fillNeutral'' actx (applySubType sub ty') id ty tyArgs
 
 {-
 NOTE: when creating a variable to place into a new neutral form, if the type is a hole, you can prioritize as many or as few
 arguments. fillNeutral'' prioritizes many arguments, and fillNeutral' prioritizes few.
 -}
---fillNeutral'' :: TypeAliasContext -> Type -> TermVarID -> Type -> List.List TypeArg -> Unify Term
---fillNeutral'' actx ty id ty' tyArgs = case ty of
---  Arrow _ ty1 ty2 ->
---    (\tm -> App defaultAppMD tm (freshHole unit) ty1 ty2)
---      <$> fillNeutral'' actx ty2 id ty' tyArgs
---  _ -> do
---    void $ normThenUnify actx ty' ty
---    pure $ Var defaultVarMD id tyArgs
+fillNeutral'' :: TypeAliasContext -> Type -> TermVarID -> Type -> List.List TypeArg -> Unify Term
+fillNeutral'' actx ty id ty' tyArgs = case ty of
+  Arrow _ ty1 ty2 ->
+    (\tm -> App defaultAppMD tm (freshHole unit) ty1 ty2)
+      <$> fillNeutral'' actx ty2 id ty' tyArgs
+  _ -> do
+    void $ normThenUnify actx ty' ty
+    pure $ Var defaultVarMD id tyArgs
 
 -- first type is that of variable, second type is that of location that variable will fill
 fillNeutral' :: TypeAliasContext -> Type -> TermVarID -> Type -> List.List TypeArg -> Unify Term
